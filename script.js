e3c7e7229a70e3e9590e9c3d268f5216c8b0aaa8
NeelamDubey <119338940+NeelamDubey@users.noreply.github.com>
2023-08-10 16:45:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,9 +6,18 @@ const person = {
 };
 
 // Write your code here, make sure the name of variables is exactly same as mentioned in the problem statement
+const team = players;
+
+// Create a copy of the 'players' array
+const team1 = [...players]; // This creates a shallow copy of the array
+
+// Create a copy of the 'person' object
+const cap1 = { ...person }; // This creates a shallow copy of the object
 
 window.players = players;
 window.person = person;
 window.team = team;
 window.team1 = team1;
 window.cap1 = cap1;
+
+
